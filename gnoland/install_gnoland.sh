9982a0209e46f0e3449a56379cbd168d2ee5fb8d
Your Name <you@example.com>
2022-06-24 19:01:01 +0700

    update

--- gnoland/install_gnoland.sh
+++ gnoland/install_gnoland.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# открытие портов
+sudo ufw default allow outgoing
+sudo ufw default deny incoming
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw allow 26657
+sudo ufw enable
+sudo ufw reload
+
 # подготовка сервера
 sudo apt update && sudo apt upgrade -y
 sudo apt install make clang pkg-config libssl-dev libclang-dev build-essential git curl ntp jq llvm tmux htop screen -y
@@ -16,46 +25,53 @@ source ~/.bash_profile
 go version
 
 # установка ноды
-cd $HOME
 git clone https://github.com/gnolang/gno/
 cd gno
 make
+cd $HOME
 
-# создаем кошелек (generate) или восстанавливаем уже существующий (--recover)
-./build/gnokey generate
-./build/gnokey add account --recover
+gnokey=$HOME/gno/.build/gnokey
 
-# список кошельков
-./build/gnokey list
+# создаем кошелек
+$gnokey add account > $HOME/gno/config.txt
+cat $HOME/gno/config.txt
 
 # установка переменной address
-read -p "Wallet Address: " address
+# address=cat $HOME/config
+# addr="$(sed '/pub:/,$ d' < $HOME/.gno/config.txt)"
+# addr=cat $HOME/.gno/config.txt | awk '{ print } $0 == "pub:" { exit(0) }'
+# cat $HOME/.gno/config.txt | awk '/pub:/{exit 0}{print $0}'
+$addr_length=expr length address;
+until [ $addr_length < 40 ]
+do
+    read -p 'Скопируйте и вставьте адрес кошелька (указан в выводе выше: "addr:" )'address
+done
 echo 'export address='$address >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 # получение токенов
-while true;
-    do curl 'https://gno.land:5050/'
-        --data-raw 'toaddr='$address;
-        ./build/gnokey query "bank/balances/"$address
-        --remote gno.land:36657;
+balance="$(./build/gnokey query "bank/balances/"$address --remote gno.land:36657)"
+while [ $balance !== 'height: 0 data: "3000gnot"' ];
+    do  curl 'https://gno.land:5050/' --data-raw 'toaddr='$address;
+        balance="$(./build/gnokey query "bank/balances/"$address --remote gno.land:36657)";
+        echo $balance;
         sleep 2;
     done
 
 
-# регистрация нашего аккаунта
+# регистрация аккаунта
 ./build/gnokey query auth/accounts/$address --remote gno.land:36657
 
 read -p "User Name: " username
+echo 'export username='$username >> $HOME/.bash_profile
 read -p "Account Number: " account_number
 read -p "Sequence Number: " sequence_number
-echo 'export username='$username >> $HOME/.bash_profile
 echo 'export account_number='$account_number >> $HOME/.bash_profile
 echo 'export sequence_number='$sequence_number >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 
-# создаем фаил, который будет содержать информацию о нашей регистрации
+# создаем фаил с информацией о нашей регистрации
 ./build/gnokey maketx call $address --pkgpath "gno.land/r/users" --func "Register" --gas-fee 1gnot --gas-wanted 3000000 --send "2000gnot" --args "" --args $username --args "" > unsigned.tx
 
 # создаем транзакцию
@@ -68,4 +84,4 @@ source $HOME/.bash_profile
 read -p "Вставьте ссылку на свою работу: " url
 echo 'export url='$url >> $HOME/.bash_profile
 source $HOME/.bash_profile
-./build/gnokey maketx call $address --pkgpath "gno.land/r/boards" --func "CreateReply" --gas-fee 1gnot --gas-wanted 3000000 --send "" --broadcast true --chainid testchain --args "1" --args "8" --args "8" --args "<URL>" --remote gno.land:36657
+$gnokey maketx call $address --pkgpath "gno.land/r/boards" --func "CreateReply" --gas-fee 1gnot --gas-wanted 3000000 --send "" --broadcast true --chainid testchain --args "1" --args "8" --args "8" --args "<URL>" --remote gno.land:36657
