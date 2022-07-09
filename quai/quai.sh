251d058d5b9a7ce7a7cb36aad0b89830be8224a0
Your Name <you@example.com>
2022-07-09 13:10:32 +0700

    update

--- quai/quai.sh
+++ quai/quai.sh
@@ -8,77 +8,6 @@ if [ !$quai ]; then
 fi
 
 
-    
-    read -p "Enter Miner Name: " STATS_NAME
-    echo 'export STATS_NAME='$STATS_NAME >> $HOME/.bash_profile
-
-    
-    source $HOME/.bash_profile
-
-
-
-
-
-    # update & upgrade system
-    sudo apt update && sudo apt upgrade -y
-
-    # install dependencies
-    sudo apt install curl build-essential git wget jq make gcc tmux -y
-
-    # install GO
-    wget -c https://golang.org/dl/go1.17.7.linux-amd64.tar.gz
-    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.7.linux-amd64.tar.gz
-    export PATH=$PATH:/usr/local/go/bin
-
-
-function installQuaiNode{
-    cd
-    # clone go-quai onto your machine
-    git clone https://github.com/spruce-solutions/go-quai
-
-    # move into go-quai directory
-    cd go-quai
-
-    setVariables
-
-    # copies environment variables to your machine
-    cp network.env.dist network.env
-
-    # generates go-quai binary
-    make go-quai
-
-}
-
-function installQuaiMiner{
-    cd
-    # clone quai-manager
-    git clone https://github.com/spruce-solutions/quai-manager
-
-    # move into quai-manager directory
-    cd quai-manager
-
-    # generate quai-manager binary
-    make quai-manager
-
-    # move into go-quai directory
-    cd go-quai
-
-    # start running our full node that is primed for mining
-    make run-full-mining
-
-    # move into quai-manager directory
-    cd quai-manager
-
-    # start mining
-    make run-mine-background region=1 zone=2
-
-}
-
-function update{
-    git pull origin main
-    make quai-manager
-}
-
 PS3='Please enter your choice (input your option number and press enter): '
 options=("Install" "Update" "Run" "Stop" "Check Logs"  )
 
@@ -86,11 +15,75 @@ select opt in "${options[@]}"
 do
     case $opt in
         "Install")
-            setVariables
-            installQuaiNode
-            installQuaiMiner
-            break
+
+            ######################## Preparation ########################
+
+            # update & upgrade system
+            sudo apt update && sudo apt upgrade -y
+
+            # install dependencies
+            sudo apt install curl build-essential git wget jq make gcc tmux -y
+
+            # install GO
+            wget -c https://golang.org/dl/go1.17.7.linux-amd64.tar.gz
+            rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.7.linux-amd64.tar.gz
+            export PATH=$PATH:/usr/local/go/bin
+            
+            
+            ######################## Install Quai Node ########################
+            
+            # go to parent directory
+            cd $HOME
+
+            # clone go-quai onto your machine
+            git clone https://github.com/spruce-solutions/go-quai
+
+            # move into go-quai directory
+            cd go-quai
+
+            if [ !STATS_NAME ]; then
+                read -p "Enter Miner Name: " STATS_NAME
+                echo 'export STATS_NAME='$STATS_NAME >> $HOME/.bash_profile  
+            fi
+            echo 'export STATS_PASS=quainetworkbronze' >> $HOME/.bash_profile  
+            source $HOME/.bash_profile
+
+            # copies environment variables to your machine
+            cp network.env.dist network.env
+
+            # generates go-quai binary
+            make go-quai
+
+            
+            ######################## Install Quai Miner ########################
+
+            # go to parent directory
+            cd $HOME
+    
+            # clone quai-manager
+            git clone https://github.com/spruce-solutions/quai-manager
+
+            # move into quai-manager directory
+            cd quai-manager
+
+            # generate quai-manager binary
+            make quai-manager
+
+            # move into go-quai directory
+            cd go-quai
+
+            # start running our full node that is primed for mining
+            make run-full-mining
+
+            # move into quai-manager directory
+            cd quai-manager
+
+            # start mining
+            make run-mine-background region=1 zone=2
+
+        break
         ;;
+
         "Update")
             update
             break
