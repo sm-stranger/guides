ad0592aa0df1ccae2b15bb7ac5d85c48eae50197
Your Name <you@example.com>
2022-08-03 10:13:43 +0700

    update

--- Near/near_rpc.sh
+++ Near/near_rpc.sh
@@ -1,13 +1,17 @@
 # prerequisites
 sudo apt update && sudo apt upgrade -y
-apt install -y git binutils-dev libcurl4-openssl-dev zlib1g-dev libdw-dev libiberty-dev cmake gcc g++ python docker.io protobuf-compiler libssl-dev pkg-config clang llvm cargo awscli
+apt install -y git binutils-dev libcurl4-openssl-dev zlib1g-dev libdw-dev libiberty-dev cmake gcc g++ python docker.io protobuf-compiler libssl-dev pkg-config clang llvm cargo awscli jq mc curl screen
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/bin
+rustc --version
+
 
 # clone nearcore project from GitHub
 git clone https://github.com/near/nearcore
 cd nearcore
 git fetch origin --tags
 
-git checkout tags/1.25.0 -b mynode
+git checkout tags/1.28.0 -b mynode
 
 # compile nearcore binary
 make release
@@ -19,13 +23,25 @@ make release
 rm ~/.near/config.json
 wget https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/mainnet/config.json -P ~/.near/
 
+# change config.json
+# "archive": true
+
 # get data backup
-aws s3 --no-sign-request cp s3://near-protocol-public/backups/mainnet/rpc/latest .
+aws s3 --no-sign-request cp s3://near-protocol-public/backups/mainnet/archive/latest .
 LATEST=$(cat latest)
-aws s3 --no-sign-request cp --no-sign-request --recursive s3://near-protocol-public/backups/mainnet/rpc/$LATEST ~/.near/data
+aws s3 --no-sign-request cp --no-sign-request --recursive s3://near-protocol-public/backups/mainnet/archive/$LATEST ~/.near/data
+
+# get data backup (with s5cmd)
+docker run --rm -v ~/.aws:/root/.aws s5cmd cp s3://near-protocol-public/backups/mainnet/archive/latest .
+LATEST=$(cat latest)
+docker run --rm -v ~/.aws:/root/.aws cp --no-sign-request --recursive s3://near-protocol-public/backups/mainnet/archive/$LATEST ~/.near/data
+
+docker run --rm -v ~/.aws:/root/.aws peakcom/s5cmd cp s3://near-protocol-public/backups/mainnet/archive/latest .
+LATEST=$(cat latest)
+docker run --rm -v ~/.aws:/root/.aws peakcom/s5cmd cp --no-sign-request --recursive s3://near-protocol-public/backups/mainnet/archive/$LATEST ~/.near/data
 
 # run the node
-./target/release/neard --home ~/.near run
+./nearcore/target/release/neard --home ~/.near run
 
 
 # screen session
@@ -41,3 +57,41 @@ screen -S near
 # Ctrl+a Ctrl+a Toggle between the current and previous windows
 # Ctrl+a Q Close all regions but the current one.
 # Ctrl+a X Close the current region.
+
+
+
+./nearcore/target/release/neard --home ~/.near
+
+
+# install npm
+
+# install with nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh -bash
+source ~/.bashrc
+nvm list-remote
+nvm install #version
+
+# install near-cli
+npm install -g near-cli
+
+# set up a global environment variable
+export NEAR_ENV=mainnet
+export NEAR_CLI_MAINNET_RPC_SERVER_URL=<put_your_rpc_server_url_here>
+
+
+# install go
+ver="1.18.2"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+go version
+
+
+# install Docker
+sudo apt install docker.io curl -y \
+&& sudo systemctl start docker \
+&& sudo systemctl enable docker
--- Near/near_rpc_request.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget -qO-  -t 1 -T 5 --post-data '{"jsonrpc": "2.0", "id":"dontcare", "method": "block", "params": {"finality": "final"}}' https://rpc.ankr.com/near | jq
